5e915bbc4ba36a953e1dd6c3b8f04c29ea71a37e
oscode04 <hellogaso@gmail.com>
2024-01-07 19:37:46 +0700

    benerin responsive di home

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -7,35 +7,37 @@
 @section('content')
     <!-- content -->
     <div class="container">
-        <div class="row">
-          <div class="col-lg-6 main-img">
+        <div class="row mt-lg-5">
+          <div class="col-lg-6 col-12 main-img">
             <img
-              class="position-absolute top-50 start-0 translate-middle-y"
+              class="mx-auto d-block"
               src="../aset/main-img.jpg"
               alt="" />
           </div>
-          <div class="col-lg-6 web-tittle">
-            <h1>
-              Selamat Datang Di
-              <br />
-              Dokter Tanaman
-            </h1>
-            <p>
-              Perawatan Cabai yang Efektif: Konsultasikan Tanaman Cabai Anda di
-              Sini!
-            </p>
-            <div class="row">
-              <div class="col-lg-5">
-                <a href="konsultasi.html">
-                  <button type="button" class="konsul-btn btn">
-                    Konsultasi Sekarang
+          <div class="col-lg-6 col-12 web-tittle my-lg-auto d-lg-block">
+            <div class="ms-3 mt-3">
+              <h1>
+                Selamat Datang Di
+                <br />
+                Dokter Tanaman
+              </h1>
+              <p>
+                Perawatan Cabai yang Efektif: Konsultasikan Tanaman Cabai Anda di
+                Sini!
+              </p>
+              <div class="row me-2">
+                <div class="col-lg-5 col-6">
+                  <a href="{{route("konsultasi")}}">
+                    <button type="button" class="konsul-btn btn">
+                      Konsultasi Sekarang
+                    </button>
+                  </a>
+                </div>
+                <div class="col-lg-6 col-6">
+                  <button type="button" class="info-btn btn">
+                    Informasi Penyakit
                   </button>
-                </a>
-              </div>
-              <div class="col-lg-6">
-                <button type="button" class="info-btn btn">
-                  Informasi Penyakit
-                </button>
+                </div>
               </div>
             </div>
           </div>
